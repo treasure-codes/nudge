f5a21a81fb590599612f01d7a8720c7cdfa3c0f1
Daniel Sobowale <danbowale@gmail.com>
2026-03-29 12:37:43 -0500

    pwa ability

--- next.config.js
+++ next.config.js
@@ -1,5 +1,48 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-style',
+        expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts-webfont',
+        expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
+      },
+    },
+    {
+      urlPattern: /^\/api\//,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'api-cache',
+        networkTimeoutSeconds: 10,
+        expiration: { maxEntries: 50, maxAgeSeconds: 60 * 5 },
+      },
+    },
+    {
+      urlPattern: /.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'nudge-cache',
+        expiration: { maxEntries: 200, maxAgeSeconds: 60 * 60 * 24 },
+      },
+    },
+  ],
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@infobip-api/sdk'],
 }
-module.exports = nextConfig
+
+module.exports = withPWA(nextConfig)
